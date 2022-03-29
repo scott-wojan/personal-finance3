eb31b790789e300cf6192a4da82f8e8bd51377cd
scott-wojan <swojan@rocketmail.com>
2022-03-29 14:30:32 -0500

    Checkpoint charlie

--- components/charts/BudgetBarChart.jsx
+++ components/charts/BudgetBarChart.jsx
@@ -11,6 +11,7 @@ import {
 import { Bar } from "react-chartjs-2";
 import faker from "@faker-js/faker";
 import { useMantineTheme } from "@mantine/core";
+import { getFormattedCurrency } from "formatting";
 
 ChartJS.register(
   CategoryScale,
@@ -21,8 +22,38 @@ ChartJS.register(
   Legend
 );
 
+const dollarAmounts = {
+  budget: [
+    { date: "01/01/2021", amount: 8824.29 },
+    { date: "02/01/2021", amount: 8832.5 },
+    { date: "03/01/2021", amount: 10536.16 },
+    { date: "04/01/2021", amount: 8834.06 },
+    { date: "05/01/2021", amount: 8930.09 },
+    { date: "06/01/2021", amount: 8992.19 },
+    { date: "07/01/2021", amount: 7430.29 },
+    { date: "08/01/2021", amount: 23676.83 },
+    { date: "09/01/2021", amount: 10749.14 },
+    { date: "10/01/2021", amount: 15560.2 },
+    { date: "11/01/2021", amount: 20999.72 },
+    { date: "12/01/2021", amount: 14451.76 },
+  ],
+  spend: [
+    { date: "01/01/2021", amount: 8724.29 },
+    { date: "02/01/2021", amount: 8832.5 },
+    { date: "03/01/2021", amount: 14536.16 },
+    { date: "04/01/2021", amount: 7834.06 },
+    { date: "05/01/2021", amount: 8430.09 },
+    { date: "06/01/2021", amount: 8892.19 },
+    { date: "07/01/2021", amount: 6430.29 },
+    { date: "08/01/2021", amount: 23576.83 },
+    { date: "09/01/2021", amount: 8749.14 },
+    { date: "10/01/2021", amount: 17560.2 },
+    { date: "11/01/2021", amount: 18999.72 },
+    { date: "12/01/2021", amount: 12451.76 },
+  ],
+};
+
 export const options = {
-  indexAxis: "y",
   elements: {
     bar: {
       borderWidth: 2,
@@ -31,31 +62,61 @@ export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: "right",
+      position: "bottom",
     },
     title: {
       display: false,
     },
   },
+  scales: {
+    xAxes: {
+      type: "time",
+      time: {
+        unit: "month", //quarter, year
+      },
+    },
+    yAxes: {
+      ticks: {
+        // Include a dollar sign in the ticks
+        callback: function (value, index, ticks) {
+          return getFormattedCurrency(value);
+        },
+      },
+    },
+  },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+// const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
 export function BudgetBarChart() {
   const theme = useMantineTheme();
 
+  const budgetData = dollarAmounts.budget.map((budget) => {
+    return {
+      x: new Date(budget.date),
+      y: budget.amount,
+    };
+  });
+
+  const spendData = dollarAmounts.spend.map((spend) => {
+    return {
+      x: new Date(spend.date),
+      y: spend.amount,
+    };
+  });
+
   const data = {
-    labels,
+    labels: [new Date("01/01/2021"), new Date("12/01/2021")],
     datasets: [
       {
         label: "Spend",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        data: spendData,
         borderColor: theme.colors.red[3],
         backgroundColor: theme.colors.red[3],
       },
       {
         label: "Goal",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        data: budgetData,
         borderColor: theme.colors.blue[3],
         backgroundColor: theme.colors.blue[3],
       },

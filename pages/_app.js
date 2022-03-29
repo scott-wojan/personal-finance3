378e1d58295b1e541edd62284076fe202f0a4710
scott-wojan <swojan@rocketmail.com>
2022-03-28 22:29:45 -0500

    X

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import React from "react";
+import { MantineProvider } from "@mantine/core";
+import { QueryClientProvider, QueryClient } from "react-query";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = (props) => {
+  const { Component, pageProps } = props;
 
-export default MyApp
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </MantineProvider>
+  );
+};
+
+export default MyApp;

3d7f9c5d77271fd470390fcf411dbf844988c1c1
scott-wojan <swojan@rocketmail.com>
2022-03-29 20:46:30 -0500

    Checkpoint charlie

--- components/accounts/AccountLineChart.jsx
+++ components/accounts/AccountLineChart.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-
+import "chartjs-adapter-moment";
 import {
   Chart as ChartJS,
   Filler,
@@ -10,11 +10,12 @@ import {
   Title,
   Tooltip,
   Legend,
+  TimeSeriesScale,
 } from "chart.js";
 
 import { Line } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
 import { useMantineTheme } from "@mantine/core";
+import { getFormattedCurrency, getFormattedDate } from "formatting";
 
 ChartJS.register(
   CategoryScale,
@@ -24,25 +25,105 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend,
-  Filler
+  Filler,
+  TimeSeriesScale
 );
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+const dollarAmounts = {
+  income: [
+    { date: "01/01/2021", amount: 8824.29 },
+    { date: "02/01/2021", amount: 8832.5 },
+    { date: "03/01/2021", amount: 104536.16 },
+    { date: "04/01/2021", amount: 8834.06 },
+    { date: "05/01/2021", amount: 8930.09 },
+    { date: "06/01/2021", amount: 8992.19 },
+    { date: "07/01/2021", amount: 7430.29 },
+    { date: "08/01/2021", amount: 23676.83 },
+    { date: "09/01/2021", amount: 10749.14 },
+    { date: "10/01/2021", amount: 15560.2 },
+    { date: "11/01/2021", amount: 20999.72 },
+    { date: "12/01/2021", amount: 14451.76 },
+  ],
+  expense: [
+    { date: "01/01/2021", amount: -36907.89 },
+    { date: "02/01/2021", amount: -17414.69 },
+    { date: "03/01/2021", amount: -30716.21 },
+    { date: "04/01/2021", amount: -105045.59 },
+    { date: "05/01/2021", amount: -28717.66 },
+    { date: "06/01/2021", amount: -13112.9 },
+    { date: "07/01/2021", amount: -10205.87 },
+    { date: "08/01/2021", amount: -15243.04 },
+    { date: "09/01/2021", amount: -14982.26 },
+    { date: "10/01/2021", amount: -12700.08 },
+    { date: "11/01/2021", amount: -9840.73 },
+    { date: "12/01/2021", amount: -18641.12 },
+  ],
+};
 
-export function AccountLineChart() {
+const incomeData = dollarAmounts.income.map((income) => {
+  return {
+    x: new Date(income.date),
+    y: income.amount,
+  };
+});
+
+const expenseData = dollarAmounts.expense.map((expense) => {
+  return {
+    x: new Date(expense.date),
+    y: expense.amount,
+  };
+});
+
+export function AccountLineChart({ numberofMonths }) {
   const theme = useMantineTheme();
   const chartRef = useRef();
 
   const [chartData, setChartData] = useState({
-    labels,
+    // labels,
     datasets: [],
   });
 
   const options = {
+    //maintainAspectRatio: false,
     responsive: true,
     plugins: {
       legend: {
         display: false,
+      },
+      tooltip: {
+        //https://www.chartjs.org/docs/latest/configuration/tooltip.html
+
+        callbacks: {
+          title: function (context) {
+            return getFormattedDate(new Date(context[0].label));
+          },
+          label: function (context) {
+            let label = context.dataset.label || "";
+            if (label) {
+              label += ": ";
+            }
+            if (context.parsed.y !== null) {
+              label += getFormattedCurrency(context.parsed.y);
+            }
+            return label;
+          },
+        },
+      },
+    },
+    scales: {
+      xAxes: {
+        type: "time",
+        time: {
+          unit: "month", //quarter, year
+        },
+      },
+      yAxes: {
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, ticks) {
+            return getFormattedCurrency(value);
+          },
+        },
       },
     },
   };
@@ -64,23 +145,25 @@ export function AccountLineChart() {
     negativeGradient.addColorStop(1, theme.fn.rgba(theme.colors.red[9], 0));
 
     const data = {
-      labels,
+      labels: [new Date("01/01/2021"), new Date("06/01/2021")],
       datasets: [
         {
-          data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+          //data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+          data: incomeData,
           borderColor: positiveGradient,
           backgroundColor: positiveGradient,
           fillColor: positiveGradient,
           fill: "start",
+          lineTension: 0.4,
         },
         {
-          data: labels.map(() =>
-            faker.datatype.number({ min: -25000, max: 1000 })
-          ),
+          //data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+          data: expenseData,
           borderColor: negativeGradient,
           backgroundColor: negativeGradient,
           fillColor: negativeGradient,
           fill: "start",
+          lineTension: 0.4,
         },
       ],
     };
@@ -90,7 +173,13 @@ export function AccountLineChart() {
 
   return (
     <div>
-      <Line ref={chartRef} height={30} options={options} data={chartData} />
+      <Line
+        ref={chartRef}
+        height={30}
+        // @ts-ignore
+        options={options}
+        data={chartData}
+      />
     </div>
   );
 }
